13692d58f0992acaa2461b562d936900ff06f0ca
modamod <modhaffer.rahmani@gmail.com>
2022-09-29 13:07:30 +0100

    Trying to deploy a sample flask app using nginx

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample-flask-app
+
+This repo is about a sample flask application that outputs ec2 instance meta-data on a slightly styled html page.
+The app will be deployed to an nginx server with the help of a ansible playbook.
---
+++ template/app/app.py
@@ -0,0 +1,21 @@
+
+from unicodedata import name
+from flask import render_template, Flask, url_for
+
+from ec2_metadata import ec2_metadata
+
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    print(url_for('index'))
+    return render_template("index.html", instance_id=ec2_metadata.instance_id, region=ec2_metadata.region)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
