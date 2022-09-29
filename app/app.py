294e9f97359f4e750ff4c7f6c6d63908c772b5e9
modamod <modhaffer.rahmani@gmail.com>
2022-09-29 15:24:33 +0100

    Updated Nginx config to pass request headers to flask
    Updated template to show where it is being accessed from

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 
 from unicodedata import name
-from flask import render_template, Flask, url_for
+from flask import render_template, Flask, url_for, request
 
 from ec2_metadata import ec2_metadata
 
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def index(path):
     print(url_for('index'))
-    return render_template("index.html", instance_id=ec2_metadata.instance_id, region=ec2_metadata.region)
+    return render_template("index.html", instance_id=ec2_metadata.instance_id, region=ec2_metadata.region, host=request.host)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
