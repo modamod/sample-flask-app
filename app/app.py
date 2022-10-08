dca7e71c0b962d1b1d906159bea418c12a2d46c9
modamod <modhaffer.rahmani@gmail.com>
2022-10-08 20:17:09 +0100

    Added custom port header to the html template

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def index(path):
     print(url_for('index'))
-    return render_template("index.html", instance_id=ec2_metadata.instance_id, region=ec2_metadata.region, host=request.host)
+    return render_template("index.html", instance_id=ec2_metadata.instance_id, region=ec2_metadata.region, host=request.host, real_port=request.headers.get("X-Real-Port"))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
